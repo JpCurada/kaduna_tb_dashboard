48d7ae1aa2b9b5aa0b1b8d890e3b15db0e29ea4f
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2024-01-23 20:29:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,14 +22,9 @@ year_quarter_options = [
     '2019 Q1', '2019 Q2', '2019 Q3', '2019 Q4', '2020 Q1', '2020 Q2',
     '2020 Q3', '2020 Q4', '2021 Q1', '2021 Q2', '2021 Q3', '2021 Q4',
     '2022 Q1', '2022 Q2', '2022 Q3', '2022 Q4', '2023 Q1', '2023 Q2',
-    '2023 Q3', '2023 Q4'
-]
+    '2023 Q3', '2023 Q4']
 
 year_quarter = st.select_slider('Year and Quarter', options=year_quarter_options)
-# selected_year = int(year_quarter.split(" ")[0])
-# adjusted_year_quarter = "2021 Q1" if selected_year < 2021 else year_quarter
-
-
 
 c1, c2 = st.columns(2)
 c1.plotly_chart(show_choropleth_for_number_of_diagnosed(year_quarter), use_container_width=True)
